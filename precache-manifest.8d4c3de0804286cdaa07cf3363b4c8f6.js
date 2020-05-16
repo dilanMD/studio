bc6b40a4b37bbbacd81727bc0c7cd4186c6762d7
dilanMD <deluxan.sgic@gmail.com>
2020-05-16 14:13:12 +0530

    Homepage build

---
+++ precache-manifest.8d4c3de0804286cdaa07cf3363b4c8f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2618bc33d566db210a0907e2428139",
+    "url": "/Lens-Art-Studio/index.html"
+  },
+  {
+    "revision": "3d646e47257068c08a69",
+    "url": "/Lens-Art-Studio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "68ace19f5b2ec8a81e16",
+    "url": "/Lens-Art-Studio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d646e47257068c08a69",
+    "url": "/Lens-Art-Studio/static/js/2.bab0a319.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Lens-Art-Studio/static/js/2.bab0a319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ace19f5b2ec8a81e16",
+    "url": "/Lens-Art-Studio/static/js/main.e2382514.chunk.js"
+  },
+  {
+    "revision": "5bfa9a10fd4f20a80b99",
+    "url": "/Lens-Art-Studio/static/js/runtime-main.47c75ece.js"
+  },
+  {
+    "revision": "8f90eb4fde0d9e5491dbd47822b9d566",
+    "url": "/Lens-Art-Studio/static/media/card.8f90eb4f.svg"
+  },
+  {
+    "revision": "4a28b5a56a3c42cadb572877469f37b2",
+    "url": "/Lens-Art-Studio/static/media/slide3.4a28b5a5.svg"
+  }
+]);
